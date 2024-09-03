31c0883542839dbee98b9b174276f949e87083bd
Alisson Rafael <132589755+AlissonRafael@users.noreply.github.com>
2024-09-03 11:10:11 -0300

    fgjjrxur

--- java.js
+++ java.js
@@ -5,20 +5,32 @@ const caixaResultado = document.querySelector("caixa-resultado");
 const textoResultado = document.querySelector("texto-resultado");
 const perguntas = [
    {
-        enunciado: "Pergunta 1",
-        alternativas: ["aternativa1","alternativa2","alternativa3"]
+        enunciado: "Quem fundou o budismo?",
+        alternativas: ["a) Confúcio","b) Siddhartha Gautama","c) Lao-Tsé"]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["aternativa1","alternativa2","alternativa3"]
+        enunciado: "Qual é o objetivo final do budismo?",
+        alternativas: ["a) Nirvana","b) Paraíso","c) Reencarnação"]
     },
     {
-        enunciado: "Pergunta 3",
-        alternativas: ["aternativa1","alternativa2","alternativa3"]
+        enunciado: "Qual é uma das Quatro Nobres Verdades?",
+        alternativas: ["a) A vida é eterna","b) A vida é sofrimento","c) A vida é uma ilusão"]
     },
     {
-        enunciado: "Pergunta 4",
-        alternativas: ["aternativa1","alternativa2","alternativa3"]
+        enunciado: "Qual é o nome do caminho que leva à cessação do sofrimento?",
+        alternativas: ["a) Caminho Triplo","b) Caminho Óctuplo","c) Caminho Duplo"]
     },
 
 ];
+
+let atual = 0;
+
+let perguntaAtual;
+ function mostraPergunta(){
+ perguntaAtual = perguntas[atual];
+
+ caixaPergunta.textContent = perguntaAtual.enunciado;
+
+ }
+
+ mostraPergunta();
